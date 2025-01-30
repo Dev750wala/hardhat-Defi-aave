04f26cdec629a64092864d3153808dc03483ac06
Dev750wala <sadisatsowaladev1@gmail.com>
2025-01-30 21:39:03 +0530

    remove Lock contract and add IWeth interface; update getWeth script to use IWeth

--- scripts/getWeth.ts
+++ scripts/getWeth.ts
@@ -6,10 +6,11 @@ export async function getWeth() {
     const signer = await ethers.getSigner(deployer)
 
     const weth = await ethers.getContractAt(
-        "WETH9",
+        "IWeth",
         "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
         signer
     )
+    
     const tx = await weth.deposit({ value: ethers.parseEther("0.01") })
     await tx.wait()
     const wethBalance = await ethers.provider.getBalance(deployer)
