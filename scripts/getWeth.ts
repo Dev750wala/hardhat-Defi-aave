62818d868a99aaa2a34e618ff653592c3945fd29
Dev750wala <sadisatsowaladev1@gmail.com>
2025-01-30 08:30:31 +0530

    initialize Hardhat project with basic configuration, sample contract, and tests

---
+++ scripts/getWeth.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+import { getNamedAccounts } from "hardhat";
+
+export async function getWeth() {
+    const { deployer } = await getNamedAccounts();
+    const signer = await ethers.getSigner(deployer)
+
+    const weth = await ethers.getContractAt(
+        "WETH9",
+        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        signer
+    )
+    const tx = await weth.deposit({ value: ethers.parseEther("0.01") })
+    await tx.wait()
+    const wethBalance = await ethers.provider.getBalance(deployer)
+
+    console.log(`WETH balance: ${ethers.formatEther(wethBalance)}`)
+}
